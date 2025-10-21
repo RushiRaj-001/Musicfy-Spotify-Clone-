003928d089970f36b14667bf61bba1ba5cafcaf9
unknown <rushirajapure111@gmail.com>
2025-10-21 17:32:17 +0530

    add JWT authentication for login and register.

--- MusifyApi/src/main/java/com/musifyApi/MusifyApi/util/JwtUtil.java
+++ MusifyApi/src/main/java/com/musifyApi/MusifyApi/util/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.musifyApi.MusifyApi.util;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +12,7 @@ import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JwtUtil {
@@ -25,7 +27,6 @@ public class JwtUtil {
         Map<String, Object> claims = new HashMap<>();
 
         claims.put("role", role);
-
         return createToken(claims,userDetails.getUsername());
     }
 
@@ -42,5 +43,35 @@ public class JwtUtil {
     private SecretKey getSigningKey() {
 
         return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String extractEmail(String jwtToken) {
+        return extractClaim(jwtToken, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean validateToken(String jwtToken, UserDetails userDetails) {
+        final String email = extractEmail(jwtToken);
+        return (email.equals(userDetails.getUsername()) && !isTokenExpired(jwtToken));
+    }
+
+    private boolean isTokenExpired(String jwtToken) {
+        return extractExpiration(jwtToken).before(new Date());
+    }
+
+    private Date extractExpiration(String jwtToken) {
+       return extractClaim(jwtToken,Claims::getExpiration);
     }
 }
